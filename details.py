c41cb4773a9e6967953e96e0c4557f5f16589710
LesokoM <shokijhb@gmail.com>
2024-10-09 18:54:42 +0200

    working on tasks

---
+++ details.py
@@ -0,0 +1,15 @@
+'''
+Ask for user name store in name variable
+Ask for user age store in age variable
+Ask for user House number store in house_num variable
+Ask for user street name store in street_name variable
+then -print out user info 
+
+'''
+name= input("What is your name? ")
+age = input("What is your age? ")
+house_num = input("What is your house number? ")
+street_name = input("What is your street name? ")
+
+
+print(f"This is {name}. She is {age} years old and lives at house number {house_num} on {street_name}")
---
+++ hello_world.py
@@ -0,0 +1,17 @@
+'''
+ask for user name 
+display the name
+ask for user age
+display the age 
+
+print out Hello World on new line 
+
+
+'''
+
+user_name = input("What is your name? ")
+print(user_name)
+user_age = input("What is your age? ")
+print(user_age)
+
+print("Hello World!")
