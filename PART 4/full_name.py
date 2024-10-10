a88d0d059a79d12352d7a359a6000f8c2e0f5ff7
LesokoM <shokijhb@gmail.com>
2024-10-10 13:30:14 +0200

    working on part 4

---
+++ PART 4/full_name.py
@@ -0,0 +1,14 @@
+
+response  = True 
+
+if response:
+    full_name  = input("Please enter full name: ")
+    if full_name == "":
+        print("You havent entered anything")
+    if len(full_name) < 4: 
+        print("You have entered less than 4 characters. Please make sure that you have entered your full name and surname.")
+    if len(full_name) >25: 
+        print(" You have entered more than 25 characters. Please make sure that you have entered your full name")
+    else: 
+        print("Thank you for entering your name")
+        response = False
