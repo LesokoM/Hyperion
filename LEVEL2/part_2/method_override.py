fb89ddd95690e251308b6311449e8ce8f6b92d49
LesokoM <shokijhb@gmail.com>
2024-11-08 10:44:03 +0200

    fixing overrride method

--- LEVEL2/part_2/method_override.py
+++ LEVEL2/part_2/method_override.py
@@ -1,37 +1,32 @@
 
 class Adult():
-    while True: 
-        try: #taking in input and making sure its correct
-            name,age,hair_colour,eye_colour = input('''Please enter the below information separated by commas:
-            name
-            age
-            hair colour
-            eye colour  
-            >>> ''').replace(" ", "").split(",")#removing any spaces and seperating input with commas
-
-            age = int(age) #making sure age is an int
-            break#breaking loop if all info is correct
-
-        except:#if user does not enter using correct fomat
-            print("Please enter information in correct format ")
+    
+    def __init__(self, name, age, eye_colour, hair_colour):
+        self.name = name
+        self.age = age
+        self.eye_colour = eye_colour
+        self.hair_colour = hair_colour
 
     def can_drive(self):
-        print(f"Name: {self.name}")
-        if self.age >= 18:#determining age of user
-            print(f"{self.name} can drive") #older than 18 can drive
-        else:
-            print(f"{self.name} can't drive") #younger than 18 cant drive
-
+       
+         print(f"{self.name} can drive") 
+       
 class Child(Adult):
 
-    def category(self):#creating function to check driving age
-        if self.age >=18:
-            person = Adult()
-            person.can_drive()
-        else:
-            person = Child()
-            person.can_drive()
+    def can_drive(self):
+        print(f"{self.name} can't drive")
 
-me = Child()
-me.category()
+name,age,hair_colour,eye_colour = input('''Please enter the below information separated by commas:
+        name
+        age
+        hair colour
+        eye colour  
+        >>> ''').replace(" ", "").split(",")#removing any spaces and seperating input with commas
+
+if age >=18:
+    me = Adult(name, age, hair_colour,eye_colour)
+else: 
+    me = Child(name, age, hair_colour,eye_colour)
+
+me.can_drive()
 
