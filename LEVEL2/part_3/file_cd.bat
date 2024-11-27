336eac4c5aa1b04d8bdfca15495357d768ed2944
LesokoM <shokijhb@gmail.com>
2024-11-27 18:31:16 +0200

    updating bugs

--- LEVEL2/part_3/file_cd.bat
+++ LEVEL2/part_3/file_cd.bat
@@ -10,6 +10,6 @@ mkdir Folder2File1
 mkdir Folder2File2
 mkdir Folder2File3
 REM deleting folders/directories
-del Folder2File2
-del Folder2File3
+rmdir Folder2File2
+rmdir Folder2File3
 
--- LEVEL2/part_3/ifExample.bat
+++ LEVEL2/part_3/ifExample.bat
@@ -1,4 +1,4 @@
-if exist new_folder (mkdir if_folder)
-
-if exist if_folder(mkdir hyperionDev) else (mkdir new-projects )
+if exist new_folder (mkdir if_folder) REM creating if_folder if new_folder exists
+REM creating hyperionDev folder if if_folder exists else creating new-projects folder
+if exist if_folder (mkdir hyperionDev) else (mkdir new-projects) 
 
