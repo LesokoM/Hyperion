437ec87ec250049045b6f2f520d7ef53d15e00a3
LesokoM <shokijhb@gmail.com>
2024-11-27 18:17:20 +0200

    adding code

---
+++ LEVEL2/part_3/file_cd.bat
@@ -0,0 +1,15 @@
+REM creating folders/directories
+mkdir Folder1 
+mkdir Folder2
+mkdir Folder3
+
+REM changing dir into folder 2
+cd Folder2
+REM creating folders/directories
+mkdir Folder2File1
+mkdir Folder2File2
+mkdir Folder2File3
+REM deleting folders/directories
+del Folder2File2
+del Folder2File3
+
